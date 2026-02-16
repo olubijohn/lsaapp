824278004049bd746a9198a1e5831aa4172e31aa
olubijohn <olubijohn4live@gmail.com>
2026-02-16 18:23:18 +0100

    chore: delete file

--- components/shared/invoice-templates/index.tsx
+++
@@ -1,7 +0,0 @@
-export { SimpleTemplate } from "./SimpleTemplate";
-export { ClassicTemplate } from "./ClassicTemplate";
-export { Modern01Template } from "./Modern01Template";
-export { Modern02Template } from "./Modern02Template";
-export { FiscalDetailsGrid } from "./FiscalDetailsGrid";
-export * from "./types";
-export * from "./utils";
--- components/shared/invoice-templates/pdf/index.tsx
+++
@@ -1,4 +0,0 @@
-export { SimplePDFTemplate } from './SimplePDFTemplate';
-export { ClassicPDFTemplate } from './ClassicPDFTemplate';
-export { Modern01PDFTemplate } from './Modern01PDFTemplate';
-export { Modern02PDFTemplate } from './Modern02PDFTemplate';
